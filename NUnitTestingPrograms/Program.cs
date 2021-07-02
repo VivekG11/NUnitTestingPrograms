f30b85f03204b9f026c515c08f66cf2f2106c5bf
VivekG11 <vivekvk2903@gmail.com>
2021-07-02 21:22:48 +0530

    [Choose an option to perform an operation]

---
+++ NUnitTestingPrograms/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NUnitTestingPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("N Unit Testing Programs.......");
+            /*.........................................
+             * Create a switch method to choose a particular operation 
+             * ............................................*/
+            Console.WriteLine("Enter an Option :");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch(option)
+            {
+                case 1:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
